ab8872633e8d7222aa99c3991fd2fb45b8f9b71d
Deynuh <60407390+Deynuh@users.noreply.github.com>
2022-11-16 22:21:19 -0800

    added js and htmls stuff

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require("path");
 
 const app = express();
 
-app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+// app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
+
+app.use("/frontend", express.static(path.resolve(__dirname, "frontend")));
 
 app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
